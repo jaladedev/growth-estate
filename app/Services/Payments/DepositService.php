2364af12ce31083eb2d606baa18f10be292e05dd
Ayodeji Alalade <ayodejialalade29@gmail.com>
2026-02-12 14:53:17 -0600

    Update DepositService.php to apply fee cap

--- app/Services/Payments/DepositService.php
+++ app/Services/Payments/DepositService.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Str;
 class DepositService
 {
     public const FEE_PERCENT = 2;
+    public const FEE_CAP_KOBO = 300000; // ₦3,000 cap in kobo
 
     /**
      * Create a deposit record
@@ -24,7 +25,10 @@ class DepositService
         $amountKobo = $amount * 100;
 
         // Calculate fee (2%)
-        $feeKobo = (int) round($amountKobo * (self::FEE_PERCENT / 100));
+        $calculatedFeeKobo = (int) round($amountKobo * (self::FEE_PERCENT / 100));
+
+        // Apply fee cap
+        $feeKobo = min($calculatedFeeKobo, self::FEE_CAP_KOBO);
 
         // Total payable
         $totalKobo = $amountKobo + $feeKobo;
